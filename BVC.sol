bdacd9357f86b60050537fdaf15388fecafffed3
bugkingK <myway0710@naver.com>
2018-05-23 10:38:42 +0900

    bvc fix

--- BVC.sol
+++ BVC.sol
@@ -1,7 +1,35 @@
 pragma solidity ^0.4.0;
 
 
+library Strings {
+
+    function concat(string _base, string _value) internal returns (string) {
+        bytes memory _baseBytes = bytes(_base);
+        bytes memory _valueBytes = bytes(_value);
+
+        string memory _tmpValue = new string(_baseBytes.length + _valueBytes.length);
+        bytes memory _newValue = bytes(_tmpValue);
+
+        uint i;
+        uint j;
+
+        for (i = 0; i < _baseBytes.length; i++) {
+            _newValue[j++] = _baseBytes[i];
+        }
+
+        for (i = 0; i < _valueBytes.length; i++) {
+            _newValue[j++] = _valueBytes[i++];
+        }
+
+        return string(_newValue);
+    }
+
+}
+
+
 contract BVC {
+
+    using Strings for string;
 
     struct Voter {
         uint phone;
@@ -54,13 +82,15 @@ contract BVC {
         string tmpCandidate;
         for (uint i = 0; i < candidateList.length; i++) {
             if (candidateList[i].placeID == _placeID) {
-                tmpCandidate += (tostring(i) + ",");
+                tmpCandidate.concat(uintToString(i));
+                tmpCandidate.concat("/");
+                
             }
         }
         return tmpCandidate;        
     }
 
-    function vote(uint _votedPlace, uint candidateCode, uint _phone) returns(bool) {
+    function vote(uint _votedPlace, uint _candidateID, uint _phone) returns(bool) {
         // 투표하기 실패 성공 여부 확인해야할까?
         // 전화번호 중복여부
         if (checkVoted() == false) {
@@ -68,10 +98,12 @@ contract BVC {
             uint voterID = voterList.length - 1;
             voterList[voterID].phone = _phone;
             voterList[voterID].votedPlace = _votedPlace;
+            candidateList[_candidateID].voteCount += 1;
             return true;
         } else {
             return false;
         }
+        
 
     }
 
@@ -99,6 +131,23 @@ contract BVC {
     function counting(uint _candidateID) returns(uint) {
         // 개표하기
         return candidateList[_candidateID].voteCount;
+    }
+       
+        //uintToString 함수
+    function uintToString(uint v) constant returns (string str) {
+        uint maxlength = 100;
+        bytes memory reversed = new bytes(maxlength);
+        uint i = 0;
+        while (v != 0) {
+            uint remainder = v % 10;
+            v = v / 10;
+            reversed[i++] = byte(48 + remainder);
+        }
+        bytes memory s = new bytes(i + 1);
+        for (uint j = 0; j <= i; j++) {
+            s[j] = reversed[i - j];
+        }
+        str = string(s);
     }
 }
 
