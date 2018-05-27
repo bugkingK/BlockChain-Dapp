94ceaf9812ff5bcd47efaa8f28bc63c20f7343f1
bugkingK <myway0710@naver.com>
2018-05-27 18:59:27 +0900

    fix)server 메소드 추가 및 json 전달방식을 전역으로 변경

--- BVC-WEB/server.js
+++ BVC-WEB/server.js
@@ -11,7 +11,7 @@ var web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider('http://yangarch.iptime.org:8545'));
 
 // sol파일의 컨트랙트 주소입니다.
-var contractAddress = '0x826b3de8319af843c1685ee1ac5f23a3acc29061';
+var contractAddress = '0xa038677892f437beb685a78e57a7495e9276774f';
 // sol파일의 abi 값입니다.
 var interface = [{"constant":false,"inputs":[{"name":"_placeID","type":"uint256"}],"name":"getCandidate","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"getAllPlace","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"voterList","outputs":[{"name":"phone","type":"uint256"},{"name":"votedPlace","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_phone","type":"uint256"},{"name":"_votedPlace","type":"uint256"}],"name":"checkVoted","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_votedPlace","type":"uint256"},{"name":"_candidateID","type":"uint256"},{"name":"_phone","type":"uint256"}],"name":"vote","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPollingPlace","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"setPollingPlace","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"placeID","type":"uint256"},{"name":"voteCount","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_placeID","type":"uint256"}],"name":"voteEnd","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"placeList","outputs":[{"name":"voting","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_placeID","type":"uint256"}],"name":"setCandidate","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_placeID","type":"uint256"}],"name":"voteStart","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"v","type":"uint256"}],"name":"uintToString","outputs":[{"name":"str","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_candidateID","type":"uint256"}],"name":"counting","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"}]
 // 컨트랙트를 연결합니다.
@@ -65,7 +65,14 @@ app.get('/getCounting', function (req, res) {
 
 
 // ------------------------- 메소드입니다 -----------------------------
-// 투표장을 생성하는 메소드입니다.
+
+var jsonString = {
+  		"code"     : "",
+        "message"  : "",
+    	"data"     : ""
+}
+
+// 1. 투표장을 생성하는 메소드입니다.
 function setPollingPlace(json){
   BVC.setPollingPlace.sendTransaction(function(err, res){
     if(!err) {
@@ -73,60 +80,78 @@ function setPollingPlace(json){
       BVC.getPollingPlace.call(function(err, res){
       	// 방금 생성한 투표장의 번호를 반환합니다.
         if(!err) {
-          console.log(res);
-          var jsonString = {
-            "result" : "",
-            "placeid" : ""
-          }
-          jsonString["result"] = "success";
-          jsonString["placeid"] = res;
-          json(jsonString);
+        	var placeid = { "placeid" : res };
+			jsonString["code"] = 200;
+			jsonString["message"] = "success";
+			jsonString["data"] = placeid;
+
+			json(jsonString);
+          
         } else {
-          console.log(err);
+			jsonString["code"] = 400;
+			jsonString["message"] = err;
+			json(jsonString);
         }
       })
 
     } else {
-      console.log(err);
-      
+      jsonString["code"] = 400;
+      jsonString["message"] = err;
+      json(jsonString);
     }
   })
 }
 
-// 후보자 등록하는 메소드입니다.
+// 2. 후보자 등록하는 메소드입니다.
 function setCandidate(placeid, json) {
 	// getCandidate로 등록한 후보자 ID 반환
 }
 
-// 등록된 투표장 보는 메소드입니다.
-function setAllplace() {
-	// getAllplace로 등록된 투표장을 볼 수 있습니다.
+// 3. 등록된 투표장 보는 메소드입니다.
+function getAllplace() {
+	var placeid = 1;
+	// set에서 get으로 
+	BVC.setAllCandidate.sendTransaction(placeid, function(err, res){
+		if(!err) {
+
+		} else {
+			console.log(err);
+		}
+	});
+	
 }
 
-// 투표하는 메소드입니다.
+// 4. 등록된 후보자보기
+function getAllCandidate(placeid, json){
+
+}
+
+// 5. 투표하는 메소드입니다.
 function setVote(placeid, candidateid, phone) {
 
 }
 
-// 투표를 했는지 확인하는 메소드입니다.
+// 6. 개표합니다.
+function getCounting(candidateid) {
+
+}
+
+// 7. 투표를 했는지 확인하는 메소드입니다.
 function getCheckVoted(phone, placeid) {
 
 }
 
-// 투표를 시작합니다.
+// 8. 투표를 시작합니다.
 function setVoteStart(placeid) {
 
 }
 
-// 투표를 종료합니다.
+// 9. 투표를 종료합니다.
 function setVoteEnd(placeid) {
 
 }
 
-// 개표합니다.
-function getCounting(candidateid) {
 
-}
 
 
 app.listen(4210, function () {
