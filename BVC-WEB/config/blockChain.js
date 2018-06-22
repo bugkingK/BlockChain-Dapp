f0b0cb0c40cb8a1295f1fd7f78cf7300bf0400ba
bugkingK <myway0710@naver.com>
2018-06-22 09:31:29 +0900

    fixing) readme

--- BVC-WEB/config/blockChain.js
+++ BVC-WEB/config/blockChain.js
@@ -16,7 +16,7 @@ var abiDefinition = JSON.parse(compiledCode.contracts[':BVC'].interface);
 // eth를 지불할 eth지갑을 선택합니다.
 web3.eth.defaultAccount = web3.eth.accounts[0];
 // sol파일의 컨트랙트 주소입니다.
-var contractAddress = '0xcedbb03af8a73546733460f87f6759564c316ba1';
+var contractAddress = '0xa4feacb0c8ad465d1edfbbc120376dc4ce8265de';
 
 // 컨트랙트를 연결합니다.
 var contract = web3.eth.contract(abiDefinition);
--- example/README.md
+++
@@ -1,8 +0,0 @@
-
-# 결과
-
-[index.html](http://bug.lasel.kr/eth/index.html)
-
-</br>
-
-[open.html](http://bug.lasel.kr/eth/open.html)
